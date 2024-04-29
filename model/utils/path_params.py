557257de1d42a8cff0d215dc1bf084965752f316
Lili1013 <wa_li_li@126.com>
2024-04-29 11:33:31 +1000

    the source code of models

---
+++ model/utils/path_params.py
@@ -0,0 +1,75 @@
+phone_elec = {
+    'source_path_inter':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/phone/phone_inter.csv',
+    'source_path_text_feat':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/phone/phone_text_feat.npy',
+    'source_path_visual_feat':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/phone/phone_visual_feat.npy',
+    'source_path_review_feat':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/phone/phone_review_feat.npy',
+    'target_path_inter':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/elec/elec_inter.csv',
+    'target_path_text_feat':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/elec/elec_text_feat.npy',
+    'target_path_visual_feat':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/elec/elec_visual_feat.npy',
+    'target_path_review_feat':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/elec/elec_review_feat.npy',
+    'save_model_path':'/home/lwang9/Data/amazon_data_process/datasets/phone_elec/model.pth',
+    'bar_length':1312
+}
+phone_sport = {
+    'source_path_inter':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/phone/phone_inter.csv',
+    'source_path_text_feat':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/phone/phone_text_feat.npy',
+    'source_path_visual_feat':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/phone/phone_visual_feat.npy',
+    'source_path_review_feat':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/phone/phone_review_feat.npy',
+    'target_path_inter':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/sport/sport_inter.csv',
+    'target_path_text_feat':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/sport/sport_text_feat.npy',
+    'target_path_visual_feat':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/sport/sport_visual_feat.npy',
+    'target_path_review_feat':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/sport/sport_review_feat.npy',
+    'save_model_path':'/data/lwang9/CDR_data_process/amazon_data_process_P2M2_CDR/datasets/phone_sport/model.pth',
+    'bar_length':240
+}
+sport_cloth = {
+    'source_path_inter':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/sport/sport_inter.csv',
+    'source_path_text_feat':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/sport/sport_text_feat.npy',
+    'source_path_visual_feat':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/sport/sport_visual_feat.npy',
+    'source_path_review_feat':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/sport/sport_review_feat.npy',
+    'target_path_inter':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/cloth/cloth_inter.csv',
+    'target_path_text_feat':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/cloth/cloth_text_feat.npy',
+    'target_path_visual_feat':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/cloth/cloth_visual_feat.npy',
+    'target_path_review_feat':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/cloth/cloth_review_feat.npy',
+    'save_model_path':'/home/lwang9/Data/amazon_data_process/datasets/sport_cloth/model.pth',
+    'bar_length':480
+}
+elec_cloth = {
+    'source_path_inter':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/elec/elec_inter.csv',
+    'source_path_text_feat':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/elec/elec_text_feat.npy',
+    'source_path_visual_feat':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/elec/elec_visual_feat.npy',
+    'source_path_review_feat':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/elec/elec_review_feat.npy',
+    'target_path_inter':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/cloth/cloth_inter.csv',
+    'target_path_text_feat':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/cloth/cloth_text_feat.npy',
+    'target_path_visual_feat':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/cloth/cloth_visual_feat.npy',
+    'target_path_review_feat':'/home/lwang9/Data/amazon_data_process/datasets/elec_cloth/cloth/cloth_review_feat.npy',
+    'save_model_path':'./home/lwang9/Data/amazon_data_process/datasets/elec_cloth/model.pth',
+    'bar_length':1013
+},
+book_movie = {
+    'source_path_inter':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/book_movie/book/book_inter.csv',
+    'source_path_text_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/book_movie/book/book_text_feat.npy',
+    'source_path_visual_feat':'',
+    'source_path_review_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/book_movie/book/book_review_feat.npy',
+    'target_path_inter':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/book_movie/movie/movie_inter.csv',
+    'target_path_text_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/book_movie/movie/movie_text_feat.npy',
+    'target_path_visual_feat':'',
+    'target_path_review_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/book_movie/movie/movie_review_feat.npy',
+    'save_model_path':'../datasets/douban/book_movie/book_movie_model.pth',
+    'bar_length':3470,
+    # 'bar_length':1740
+}
+movie_music = {
+    'source_path_inter':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/movie_music/movie/movie_inter.csv',
+    'source_path_text_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/movie_music/movie/movie_text_feat.npy',
+    'source_path_visual_feat':'',
+    'source_path_review_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/movie_music/movie/movie_review_feat.npy',
+    'target_path_inter':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/movie_music/music/music_inter.csv',
+    'target_path_text_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/movie_music/music/music_text_feat.npy',
+    'target_path_visual_feat':'',
+    'target_path_review_feat':'/data/lwang9/CDR_data_process/douban_data_process_P2M2_CDR/datasets/movie_music/music/music_review_feat.npy',
+    'save_model_path':'../datasets/douban/movie_music/movie_music_model.pth',
+    'bar_length':2730,
+    # 'bar_length':1367
+}
+
